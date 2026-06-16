7731fe18efd3bb83995444998c47cfb39c79c8c4
pouya babaei <p.babaei@kcz.local>
2026-06-16 15:40:05 +0330

    fix helpers bug

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,20 @@
 <?php
+
 use App\Models\Setting;
 
-function setting($key, $default = null)
-{
-    $item = Setting::where('key', $key)->first();
-    return $item ? $item->value : $default;
+if (!function_exists('setting')) {
+
+    function setting($key, $default = null)
+    {
+        static $cache = [];
+
+        if (array_key_exists($key, $cache)) {
+            return $cache[$key];
+        }
+
+        $value = \App\Models\Setting::where('key', $key)->value('value');
+
+        return $cache[$key] = $value ?? $default;
+    }
+
 }
